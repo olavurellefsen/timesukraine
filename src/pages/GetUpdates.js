31ca5042a3f8ef8bf452a04ff33cd0e71dce6c6f
Olexandr@MS <obal@dtu.dk>
2018-12-01 23:57:12 +0100

    Removed all the descriptive text

--- src/pages/GetUpdates.js
+++ src/pages/GetUpdates.js
@@ -33,10 +33,7 @@ const BodyLink = styled.a`
 
 export default () => (
   <AboutContainer>
-    <AboutHeader>Abonnér på opdateringer</AboutHeader>
-    <AboutBody>
-      Follow os on Twitter <BodyLink href='https://twitter.com/timesukraine'>@timesukraine</BodyLink> to get notification of new data, new scenarios or other changes.
-    </AboutBody>
-    <AboutBody><Link to='/'>Back to the front page</Link></AboutBody>
+	<AboutHeader>Subscribe to updates</AboutHeader>
+    <AboutBody><Link to='/'>To the front page</Link></AboutBody>
   </AboutContainer>
 )
