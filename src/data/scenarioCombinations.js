a903060d20d4c9f2867725d87ab66243cf1d3bf2
Olexandr@MS <obal@dtu.dk>
2019-03-15 14:35:07 +0100

    Updated scenario names

--- src/data/scenarioCombinations.js
+++ src/data/scenarioCombinations.js
@@ -1,11 +1,13 @@
 export default {scenarioCombinations : {scenarioOptions : [
-{ "id": 0, "name": "UDEC_B_0A00000_00000_00-v01", "short_description": "Before NES", "ultra_short_description": "" },
-{ "id": 1, "name": "UDEC_S_PAE0T5N_00000_00-v01", "short_description": "NES - Low Renewables", "ultra_short_description": "" },
-{ "id": 2, "name": "UDEC_S_PAETT00_00000_00-v01", "short_description": "NES - Nuclear Delay", "ultra_short_description": "" },
-{ "id": 3, "name": "UDEC_S_PAETT0N_00000_00-v01", "short_description": "Reference NES", "ultra_short_description": "" },
-{ "id": 4, "name": "UDEC_S_PAETT0N_F0000_00-v01", "short_description": "NES - Forecasting", "ultra_short_description": "" },
-{ "id": 5, "name": "UDEC_S_PCETT0N_00000_00-v01", "short_description": "NES - Coal Backup", "ultra_short_description": "" },
-{ "id": 6, "name": "UDEC_S_PEETT0N_00000_00-v01", "short_description": "NES - not Coal Backup", "ultra_short_description": "" },
-{ "id": 7, "name": "UDEC_S_PAETT0N_HTL00_00-v01", "short_description": "NES and Strategies", "ultra_short_description": "" },
-{ "id": 8, "name": "UDEC_S_PAETT0N_HTL00_80-v01", "short_description": "NES and Beyond Strategies", "ultra_short_description": "" }
+{ "id": 0, "name": "UDEC_B_0A00000_00000_00-v01", "short_description": "Frozen Policy", "ultra_short_description": "Frozen Policy" },
+{ "id": 1, "name": "division_line", "short_description": "_______________", "ultra_short_description": "_____"},
+{ "id": 2, "name": "UDEC_S_PAETT0N_00000_00-v01", "short_description": "Reference", "ultra_short_description": "Reference" },
+{ "id": 3, "name": "UDEC_S_PAE0T5N_00000_00-v01", "short_description": "- Low RE Growth", "ultra_short_description": "- Low RE Growth" },
+{ "id": 4, "name": "UDEC_S_PAETT00_00000_00-v01", "short_description": "- No New Nuclear in 2025", "ultra_short_description": "- No New Nuclear in 2025" },
+{ "id": 5, "name": "UDEC_S_PEETT0N_00000_00-v01", "short_description": "- New Balancing Techs", "ultra_short_description": "- New Balancing Techs" },
+{ "id": 6, "name": "UDEC_S_PCETT0N_00000_00-v01", "short_description": "- Green-Coal Paradox", "ultra_short_description": "- Green-Coal Paradox" },
+{ "id": 7, "name": "UDEC_S_PAETT0N_F0000_00-v01", "short_description": "- Optimise Balancing", "ultra_short_description": "- Optimise Balancing" },
+{ "id": 8, "name": "division_line", "short_description": "_______________", "ultra_short_description": "_____"},
+{ "id": 9, "name": "UDEC_S_PAETT0N_HTL00_00-v01", "short_description": "National Strategies", "ultra_short_description": "- National Strategies" },
+{ "id": 10, "name": "UDEC_S_PAETT0N_HTL00_80-v01", "short_description": "Low Carbon Society", "ultra_short_description": "- Low Carbon Society" }
 ]}};
