9c15c9f2ecbb8e29104e30a75eb863b834359160
Olexandr@MS <obal@dtu.dk>
2019-03-27 10:13:21 +0100

    Further adjustment of scenario names for the mobile version

--- src/data/scenarioCombinations.js
+++ src/data/scenarioCombinations.js
@@ -2,12 +2,12 @@ export default {scenarioCombinations : {scenarioOptions : [
 { "id": 0, "name": "UDEC_N_0A00000_00000_00-v01", "short_description": "Frozen Policy", "ultra_short_description": "Frozen Policy" },
 { "id": 1, "name": "division_line", "short_description": "_______________", "ultra_short_description": "_____"},
 { "id": 2, "name": "UDEC_N_PAETT0N_0000I_00-v01", "short_description": "Reference", "ultra_short_description": "Reference" },
-{ "id": 3, "name": "UDEC_N_PAETT00_L000I_00-v01", "short_description": "- Low RE Growth", "ultra_short_description": "- Low RE" },
-{ "id": 4, "name": "UDEC_N_PAETT00_N000I_00-v01", "short_description": "- No New Nuclear in 2025", "ultra_short_description": "- Delayed Nuc" },
-{ "id": 5, "name": "UDEC_N_PEETT0N_B0000_00-v01", "short_description": "- New Balancing Techs", "ultra_short_description": "- New Techs" },
-{ "id": 6, "name": "UDEC_N_PCETT0N_00000_00-v01", "short_description": "- Green-Coal Paradox", "ultra_short_description": "- Gr-Coal Paradox" },
-{ "id": 7, "name": "UDEC_N_PEETT0N_F0000_00-v01", "short_description": "- Optimise Balancing", "ultra_short_description": "- Opti Balancing" },
+{ "id": 3, "name": "UDEC_N_PAETT00_L000I_00-v01", "short_description": "- Low RE Growth", "ultra_short_description": "-Low RE" },
+{ "id": 4, "name": "UDEC_N_PAETT00_N000I_00-v01", "short_description": "- No New Nuclear in 2025", "ultra_short_description": "-Nuclear" },
+{ "id": 5, "name": "UDEC_N_PEETT0N_B0000_00-v01", "short_description": "- New Balancing Techs", "ultra_short_description": "-New Techs" },
+{ "id": 6, "name": "UDEC_N_PCETT0N_00000_00-v01", "short_description": "- Green-Coal Paradox", "ultra_short_description": "-GC Paradox" },
+{ "id": 7, "name": "UDEC_N_PEETT0N_F0000_00-v01", "short_description": "- Optimise Balancing", "ultra_short_description": "-Balancing" },
 { "id": 8, "name": "division_line", "short_description": "_______________", "ultra_short_description": "_____"},
-{ "id": 9, "name": "UDEC_N_PEETT0N_F0000_NS-v02", "short_description": "National Strategies", "ultra_short_description": "- UA Strategies" },
-{ "id": 10, "name": "UDEC_N_PEETT0N_F0000_LC-v01", "short_description": "Low Carbon Society", "ultra_short_description": "- Low Carbon" }
+{ "id": 9, "name": "UDEC_N_PEETT0N_F0000_NS-v02", "short_description": "National Strategies", "ultra_short_description": "-UA-Strategies" },
+{ "id": 10, "name": "UDEC_N_PEETT0N_F0000_LC-v01", "short_description": "Low Carbon Society", "ultra_short_description": "-Low Carbon" }
 ]}};
