2eb71bec11b322da1aa72b797463cab3590c45e0
Olexandr@MS <obal@dtu.dk>
2019-03-27 07:43:58 +0100

    Added Re share to the TPES chart

--- src/data/line.js
+++ src/data/line.js
@@ -2,45 +2,45 @@ export default {
   "data": {
     "scenarios": [
       {
-        "scenario": "UDEC_BAU2050_01",
+        "scenario": "UDEC_N_0A00000_00000_00-v01",
         "indicators": [
           {
-            "indicator": "web_CO2-eq Emissions by Sector",
+            "indicator": "web1_TPES",
             "indicatorGroups": [
               {
-                "indicatorGroup": "VE andel",
+                "indicatorGroup": "RE Share",
                 "indicatorGroupValues": [
                   {
                     "year": 2015,
-                    "total": 0.331877146539465
+                    "total": 0.0227468192
                   },
                   {
                     "year": 2020,
-                    "total": 0.420421573930394
+                    "total": 0.0355768536
                   },
                   {
                     "year": 2025,
-                    "total": 0.542929360548893
+                    "total": 0.0472861946
                   },
                   {
                     "year": 2030,
-                    "total": 0.688656507102247
+                    "total": 0.0617878542
                   },
                   {
                     "year": 2035,
-                    "total": 0.783682103820524
+                    "total": 0.0735481416
                   },
                   {
                     "year": 2040,
-                    "total": 0.830474711124826
+                    "total": 0.0990758007
                   },
                   {
                     "year": 2045,
-                    "total": 0.860093708777682
+                    "total": 0.122772729
                   },
                   {
                     "year": 2050,
-                    "total": 0.911501594557868
+                    "total": 0.1704456304
                   }
                 ]
               }
@@ -49,45 +49,374 @@ export default {
         ]
       },
       {
-        "scenario": "UDEC_BAU2050_ILED_01",
+        "scenario": "UDEC_N_PAETT00_L000I_00-v01",
         "indicators": [
           {
-            "indicator": "web_CO2-eq Emissions by Sector",
+            "indicator": "web1_TPES",
             "indicatorGroups": [
               {
-                "indicatorGroup": "VE andel",
+                "indicatorGroup": "RE Share",
                 "indicatorGroupValues": [
                   {
                     "year": 2015,
-                    "total": 0.331705577824873
+                    "total": 0.0227468192
                   },
                   {
                     "year": 2020,
-                    "total": 0.403497289072962
+                    "total": 0.0928758568
                   },
                   {
                     "year": 2025,
-                    "total": 0.504522332916603
+                    "total": 0.1570428392
                   },
                   {
                     "year": 2030,
-                    "total": 0.675127054971066
+                    "total": 0.2032804732
                   },
                   {
                     "year": 2035,
-                    "total": 0.758370050252607
+                    "total": 0.2775415109
                   },
                   {
                     "year": 2040,
-                    "total": 0.816201538034311
+                    "total": 0.2769592225
                   },
                   {
                     "year": 2045,
-                    "total": 0.845484900668138
+                    "total": 0.2730703473
                   },
                   {
                     "year": 2050,
-                    "total": 0.919861510947817
+                    "total": 0.2759266316
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "scenario": "UDEC_N_PAETT00_N000I_00-v01",
+        "indicators": [
+          {
+            "indicator": "web1_TPES",
+            "indicatorGroups": [
+              {
+                "indicatorGroup": "RE Share",
+                "indicatorGroupValues": [
+                  {
+                    "year": 2015,
+                    "total": 0.0227468192
+                  },
+                  {
+                    "year": 2020,
+                    "total": 0.0914491834
+                  },
+                  {
+                    "year": 2025,
+                    "total": 0.1539364614
+                  },
+                  {
+                    "year": 2030,
+                    "total": 0.2053383454
+                  },
+                  {
+                    "year": 2035,
+                    "total": 0.2779632299
+                  },
+                  {
+                    "year": 2040,
+                    "total": 0.2830182739
+                  },
+                  {
+                    "year": 2045,
+                    "total": 0.2763938125
+                  },
+                  {
+                    "year": 2050,
+                    "total": 0.3222796683
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "scenario": "UDEC_N_PAETT0N_0000I_00-v01",
+        "indicators": [
+          {
+            "indicator": "web1_TPES",
+            "indicatorGroups": [
+              {
+                "indicatorGroup": "RE Share",
+                "indicatorGroupValues": [
+                  {
+                    "year": 2015,
+                    "total": 0.0227468192
+                  },
+                  {
+                    "year": 2020,
+                    "total": 0.0913540133
+                  },
+                  {
+                    "year": 2025,
+                    "total": 0.1534940692
+                  },
+                  {
+                    "year": 2030,
+                    "total": 0.2053715815
+                  },
+                  {
+                    "year": 2035,
+                    "total": 0.2779768109
+                  },
+                  {
+                    "year": 2040,
+                    "total": 0.2831663
+                  },
+                  {
+                    "year": 2045,
+                    "total": 0.2763862427
+                  },
+                  {
+                    "year": 2050,
+                    "total": 0.3224302462
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "scenario": "UDEC_N_PCETT0N_00000_00-v01",
+        "indicators": [
+          {
+            "indicator": "web1_TPES",
+            "indicatorGroups": [
+              {
+                "indicatorGroup": "RE Share",
+                "indicatorGroupValues": [
+                  {
+                    "year": 2015,
+                    "total": 0.0231105758
+                  },
+                  {
+                    "year": 2020,
+                    "total": 0.091512868
+                  },
+                  {
+                    "year": 2025,
+                    "total": 0.1564736438
+                  },
+                  {
+                    "year": 2030,
+                    "total": 0.2052275929
+                  },
+                  {
+                    "year": 2035,
+                    "total": 0.2778078311
+                  },
+                  {
+                    "year": 2040,
+                    "total": 0.283689096
+                  },
+                  {
+                    "year": 2045,
+                    "total": 0.2765917024
+                  },
+                  {
+                    "year": 2050,
+                    "total": 0.3205935344
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "scenario": "UDEC_N_PEETT0N_B0000_00-v01",
+        "indicators": [
+          {
+            "indicator": "web1_TPES",
+            "indicatorGroups": [
+              {
+                "indicatorGroup": "RE Share",
+                "indicatorGroupValues": [
+                  {
+                    "year": 2015,
+                    "total": 0.0231610849
+                  },
+                  {
+                    "year": 2020,
+                    "total": 0.0925387173
+                  },
+                  {
+                    "year": 2025,
+                    "total": 0.1556909648
+                  },
+                  {
+                    "year": 2030,
+                    "total": 0.2067987603
+                  },
+                  {
+                    "year": 2035,
+                    "total": 0.2747279943
+                  },
+                  {
+                    "year": 2040,
+                    "total": 0.2692606318
+                  },
+                  {
+                    "year": 2045,
+                    "total": 0.2764676413
+                  },
+                  {
+                    "year": 2050,
+                    "total": 0.3139209828
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "scenario": "UDEC_N_PEETT0N_F0000_00-v01",
+        "indicators": [
+          {
+            "indicator": "web1_TPES",
+            "indicatorGroups": [
+              {
+                "indicatorGroup": "RE Share",
+                "indicatorGroupValues": [
+                  {
+                    "year": 2015,
+                    "total": 0.0231105758
+                  },
+                  {
+                    "year": 2020,
+                    "total": 0.0913476653
+                  },
+                  {
+                    "year": 2025,
+                    "total": 0.1540812405
+                  },
+                  {
+                    "year": 2030,
+                    "total": 0.2051424406
+                  },
+                  {
+                    "year": 2035,
+                    "total": 0.2754296984
+                  },
+                  {
+                    "year": 2040,
+                    "total": 0.2731426099
+                  },
+                  {
+                    "year": 2045,
+                    "total": 0.2793130099
+                  },
+                  {
+                    "year": 2050,
+                    "total": 0.3220667734
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "scenario": "UDEC_N_PEETT0N_F0000_LC-v01",
+        "indicators": [
+          {
+            "indicator": "web1_TPES",
+            "indicatorGroups": [
+              {
+                "indicatorGroup": "RE Share",
+                "indicatorGroupValues": [
+                  {
+                    "year": 2015,
+                    "total": 0.0231610849
+                  },
+                  {
+                    "year": 2020,
+                    "total": 0.0918050867
+                  },
+                  {
+                    "year": 2025,
+                    "total": 0.1548220134
+                  },
+                  {
+                    "year": 2030,
+                    "total": 0.2119371923
+                  },
+                  {
+                    "year": 2035,
+                    "total": 0.2857285224
+                  },
+                  {
+                    "year": 2040,
+                    "total": 0.2818174632
+                  },
+                  {
+                    "year": 2045,
+                    "total": 0.2840485068
+                  },
+                  {
+                    "year": 2050,
+                    "total": 0.3568453474
+                  }
+                ]
+              }
+            ]
+          }
+        ]
+      },
+      {
+        "scenario": "UDEC_N_PEETT0N_F0000_NS-v02",
+        "indicators": [
+          {
+            "indicator": "web1_TPES",
+            "indicatorGroups": [
+              {
+                "indicatorGroup": "RE Share",
+                "indicatorGroupValues": [
+                  {
+                    "year": 2015,
+                    "total": 0.0231610849
+                  },
+                  {
+                    "year": 2020,
+                    "total": 0.093751483
+                  },
+                  {
+                    "year": 2025,
+                    "total": 0.1552106269
+                  },
+                  {
+                    "year": 2030,
+                    "total": 0.2068146416
+                  },
+                  {
+                    "year": 2035,
+                    "total": 0.2853113595
+                  },
+                  {
+                    "year": 2040,
+                    "total": 0.2816401199
+                  },
+                  {
+                    "year": 2045,
+                    "total": 0.2795134585
+                  },
+                  {
+                    "year": 2050,
+                    "total": 0.3191746196
                   }
                 ]
               }
